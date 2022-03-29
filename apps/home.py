3fec9eb6cef8c526999d77ce0f580e9a0ad6f29a
TurquoiseHexagonSun <j.brookes@ufl.edu>
2022-03-29 05:42:28 -0400

    Main page and State updates
    Added the sidebar and map to the main page and changed the layout of the state page.

--- apps/home.py
+++ apps/home.py
@@ -1,17 +1,112 @@
+from itertools import count
 import streamlit as st
 from hydralit import HydraHeadApp
 from db import db_conn as oracle_db
+import numpy as np
+import pandas as pd
+from PIL import Image
+import pydeck as pdk
+import datetime
+import altair as alt
+
 
 class Home(HydraHeadApp):
 
     def run(self):
         
-        # Example code to get data from database using cursor and connection object
+        l1 = Image.open('images/logo.png')
+        l2 = Image.open('images/logo2.png')
+        st.image(l1)
 
-        st.title('Continent(s) From Encompasses table')
-        cursor = oracle_db.connection.cursor()
+        """
+        This section is for the elements in the sidebar
+        """
+        st.sidebar.image(l2, width = 250)
 
-        cursor.execute("""SELECT DISTINCT continent
-                    FROM encompasses""")
-        for row in cursor:
-            st.text(row[0])
+        # Date selection
+        day = 'Accidents by Day'
+        st.sidebar.header(day, anchor = None)
+        add_calendar = st.sidebar.date_input(
+            "Date:", datetime.date(2019, 4, 1)
+        )
+
+        # Year slider
+        st.sidebar.header('Accidents by Year', anchor = None)
+        add_slider = st.sidebar.slider(
+            'Select the range of years',
+            2016, 2021, (2016, 2017)
+        )
+        
+        # Weather options
+        st.sidebar.header('Select Weather Conditions', anchor = None)
+        w_clear = st.sidebar.checkbox('Clear')
+        w_rain = st.sidebar.checkbox('Rain')
+        w_snow = st.sidebar.checkbox('Snow')
+        w_tornado = st.sidebar.checkbox('Tornado')
+
+        # Temperature options
+        st.sidebar.header('Select Temperature', anchor = None)
+        temp1 = st.sidebar.checkbox('00 - 34 °F')
+        temp2 = st.sidebar.checkbox('35 - 69 °F')
+        temp3 = st.sidebar.checkbox('70 - 100 °F')
+
+        # Time selection
+        st.sidebar.header('Select Time', anchor = None)
+        st.sidebar.time_input('Select Time', datetime.time(8, 45))
+
+        # Text box
+        st.sidebar.header('Traffic Pro', anchor = None)
+        txt = st.sidebar.text_area('About', 
+            "TrafficPro is a traffic accident data visualization tool designed for"
+            "professionals in many fields from public safety to civil engineering."
+            "TrafficPro allows users to analyze trends about traffic accidents"
+            "occurring in the United States from 2016-2019."
+    
+            "TrafficPro integrates this accident data along with state demographic"
+            "data to allow users to visualize trends in areas of interest.",
+            height = 300
+        )
+
+        """
+        This section is for the main page elements
+        """
+        # Data frame and map
+        df = pd.DataFrame(
+            np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+            columns=['lat', 'lon']
+        )
+
+        st.pydeck_chart(pdk.Deck(
+            map_style = 'mapbox://styles/mapbox/light-v9',
+            initial_view_state = pdk.ViewState(
+                latitude = 37.0,
+                longitude = -98.0,
+                zoom = 3,
+                pitch = 10,
+            ),
+            layers = [
+                pdk.Layer(
+                    'ScatterplotLayer',
+                    data = df,
+                    get_position = '[lon, lat]',
+                    get_color = '[200, 30, 0, 160]',
+                    get_radius = 75,
+                ),
+            ],
+        ))
+
+        # Data frame and bar graph
+        df_graph = pd.DataFrame({
+            'State': ['Florida', 'Michigan', 'Texas', 'Arizona', 'Nevada', 
+                    'NY', 'Georgia', 'Maryland', 'California', 'New Mexico'],
+            'Accident Totals': [450000, 250000, 105345, 500450, 320032, 
+                                75345, 350450, 320032, 145345, 600450]
+        })
+        chart_data = alt.Chart(df_graph).mark_bar().encode(
+            x = 'State', 
+            y = 'Accident Totals',
+            color = 'Origin:N'
+        ).properties(height = 500, title = "Bar Graph").configure_range(
+            category={'scheme': 'yelloworangered'}
+        )
+        st.altair_chart(chart_data, use_container_width = True)
--- apps/state.py
+++ apps/state.py
@@ -7,53 +7,60 @@ import pandas as pd
 import numpy as np
 import datetime
 import altair as alt
+from PIL import Image
+
 
 class State(HydraHeadApp):
 
     def run(self):
 
-        # Dropdown selection
-        state = 'State'
-        st.sidebar.header(state, anchor=None)
+        # logos
+        l1 = Image.open('images/logo.png')
+        l2 = Image.open('images/logo2.png')
+        st.image(l1)
+        st.sidebar.image(l2, width = 250)
+        
+        """
+        This section is for the elements in the sidebar
+        """
+        # State selection
+        st.sidebar.header('State', anchor=None)
         add_selectbox = st.sidebar.selectbox(
             "Select State",
             ('Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 
             'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 
             'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 
             'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 
-            'Missouri', 'MontanaNebraska', 'Nevada', 'NewHampshire', 'NewJersey', 
-            'NewMexico', 'NewYork', 'NorthCarolina', 'NorthDakota', 'Ohio', 'Oklahoma', 
-            'Oregon', 'PennsylvaniaRhodeIsland', 'SouthCarolina', 'SouthDakota', 
+            'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'NewJersey', 
+            'NewMexico', 'NewYork', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 
+            'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 
             'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 
-            'WestVirginia', 'Wisconsin', 'Wyoming') 
+            'West Virginia', 'Wisconsin', 'Wyoming') 
         )
 
         # Date selection
         day = 'Accidents by Day'
-        st.sidebar.header(day, anchor=None)
+        st.sidebar.header(day, anchor = None)
         add_calendar = st.sidebar.date_input(
             "Date:", datetime.date(2019, 4, 1)
         )
 
-        # year slider
-        year = 'Accidents by Year'
-        st.sidebar.header(year, anchor=None)
+        # Year slider
+        st.sidebar.header('Accidents by Year', anchor = None)
         add_slider = st.sidebar.slider(
             'Select the range of years',
             2016, 2021, (2016, 2017)
         )
 
         # multiselect weather
-        weather = 'Weather'
-        st.sidebar.header(weather, anchor=None)
+        st.sidebar.header('Weather', anchor = None)
         add_multiselect = st.sidebar.multiselect(
             'Select Weather Condition',
             ['Rain', 'Snow', 'Partly Cloudy', 'Tornado']
         )
 
         # multiselect temperature
-        temperature = 'Temperature'
-        st.sidebar.header(temperature, anchor=None)
+        st.sidebar.header('Temperature', anchor = None)
         add_multiselect = st.sidebar.multiselect(
             'Select Temperature',
             ['00 - 34 °F', '35 - 69 °F', '70 - 100 °F']
@@ -61,64 +68,76 @@ class State(HydraHeadApp):
 
         # multiselect time
         time = 'Time'
-        st.sidebar.header(time, anchor=None)
+        st.sidebar.header(time, anchor = None)
         add_multiselect = st.sidebar.multiselect(
             'Select Time',
             ['12:00 AM - 05:59 AM', '06:00 AM - 11:59 AM', 
             '12:00 PM - 05:59 PM', '06:00 PM - 11:59 PM']
         )
 
-        # user input
-        name = st.text_input("Enter a city name")
+        """
+        This section is for the main page elements
+        """
+        # creates a two column layout. col1 holds the map
+        # col2 holds the table
+        col1, col2 = st.columns(2)
+        
+        with col1:
+             # user input
+            name = st.text_input("Enter a city name")
 
-        # query the city input by user
-        cursor = oracle_db.connection.cursor()
-        sql = """SELECT * FROM city WHERE name = :city_name"""
-        cursor.execute(sql, city_name = name)
-        for row in cursor:
-            st.text(row)
-        
-        l1 = 0.0
-        l2 = 0.0
+            # query the city input by user
+            cursor = oracle_db.connection.cursor()
+            sql = """SELECT * FROM city WHERE name = :city_name"""
+            cursor.execute(sql, city_name = name)
+            for row in cursor:
+                st.text(row)
+            
+            l1 = 0.0
+            l2 = 0.0
 
-        # get the latitude
-        latitude = """SELECT latitude FROM city WHERE name = :city_name"""
-        cursor.execute(latitude, city_name = name)
-        for row in cursor:
-            lat = row[0]
-            l1 = float(lat)
-        
-        # get the longitude
-        longitude = """SELECT longitude FROM city WHERE name = :city_name"""
-        cursor.execute(longitude, city_name = name)
-        for row in cursor:
-            long = row[0]
-            l2 = float(long)
-        
-        # updates and displays the city that matches the latitude and longitude
-        df = pd.DataFrame({
-            'City ' : [name], 
-            'lat' : [l1],
-            'lon' : [l2]
+            # get the latitude
+            latitude = """SELECT latitude FROM city WHERE name = :city_name"""
+            cursor.execute(latitude, city_name = name)
+            for row in cursor:
+                lat = row[0]
+                l1 = float(lat)
+            
+            # get the longitude
+            longitude = """SELECT longitude FROM city WHERE name = :city_name"""
+            cursor.execute(longitude, city_name = name)
+            for row in cursor:
+                long = row[0]
+                l2 = float(long)    
+
+            # data frame and map
+            df_map = pd.DataFrame({
+                'City ' : [name], 
+                'lat' : [l1],
+                'lon' : [l2]
+            })
+            st.map(df_map, zoom = 10)
+
+        with col2:
+            st.header('State Data', anchor = None)
+            with st.expander("See details"):
+                st.write('Add some additional text here')
+                
+            df_table = pd.DataFrame(
+                np.random.randn(10, 5),
+                columns=('col %d' % i for i in range(5))
+            )
+            st.table(df_table)
+
+        # data frame and bar graph
+        df_graph = pd.DataFrame({
+            'State': ['Florida', 'Michigan', 'Texas', 'Arizona', 'Nevada', 
+                    'NY', 'Georgia', 'Maryland', 'California', 'New Mexico'],
+            'Accident Totals': [450000, 250000, 105345, 500450, 320032, 
+                                75345, 350450, 320032, 145345, 600450]
         })
-        st.map(df, zoom = 10)
-
-        #creating a sample dataframe
-        #data_set = np.random.randint(1,10,(5,4))
-        data_set = {
-            'State': ['Florida', 'Michigan', 'Texas', 'Arizona', 'Nevada', 'NY', 'Georgia', 'Maryland', 'California', 'New Mexico'],
-            'Accident Totals': [450000, 250000, 105345, 500450, 320032, 75345, 350450, 320032, 145345, 600450]
-        }
-
-        dframe = pd.DataFrame(data_set)
-
-        #specifying the figure to plot
-        chart_data = alt.Chart(dframe).mark_bar().encode(
+        chart_data = alt.Chart(df_graph).mark_bar().encode(
             x = 'State', 
             y = 'Accident Totals'
         ).properties(height = 500, title = "Bar Graph")
-        #plotting the figure
         st.altair_chart(chart_data, use_container_width = True)
-        
-
-    
