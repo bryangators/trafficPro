ee9588a6bea868ea7ee35818c4c043f4c83ed2dd
TurquoiseHexagonSun <j.brookes@ufl.edu>
2022-04-04 07:10:04 -0400

    Database totals
    Added a button that gets the number of tuples in each of the tables. displays the totals in a text box.

--- apps/home.py
+++ apps/home.py
@@ -12,6 +12,93 @@ import altair as alt
 
 class Home(HydraHeadApp):
 
+    # button for tuples in the database
+    def dbstats(self):
+        
+        cursor = oracle_db.connection.cursor()
+        st.sidebar.header('Traffic Pro Database Information', anchor = None)
+        
+        if st.sidebar.button('Click For Totals'):
+            q1 = """SELECT COUNT(*) 
+                    FROM "J.POULOS".Accident"""
+            cursor.execute(q1)
+            for row in cursor:
+                q1t= row[0]
+            
+            q2 = """SELECT COUNT(*) 
+                    FROM "J.POULOS".State"""
+            cursor.execute(q2)
+            for row in cursor:
+                q2t= row[0]
+
+            q3 = """SELECT COUNT(*) 
+                    FROM "J.POULOS".State_pop"""
+            cursor.execute(q3)
+            for row in cursor:
+                q3t= row[0]
+
+            q4 = """SELECT COUNT(*) 
+                    FROM "J.POULOS".State_fund"""
+            cursor.execute(q4)
+            for row in cursor:
+                q4t= row[0]    
+
+            q5 = """SELECT COUNT(*) 
+                    FROM "J.POULOS".State_veh"""
+            cursor.execute(q5)
+            for row in cursor:
+                q5t= row[0]
+
+            q6 = """SELECT COUNT(*) 
+                    FROM "J.POULOS".State_lic_drivers"""
+            cursor.execute(q6)
+            for row in cursor:
+                q6t= row[0]    
+
+            q7 = """SELECT COUNT(*) 
+                    FROM "J.POULOS".County"""
+            cursor.execute(q7)
+            for row in cursor:
+                q7t= row[0]   
+
+            q8 = """SELECT COUNT(*) 
+                    FROM "J.POULOS".County_pop"""
+            cursor.execute(q8)
+            for row in cursor:
+                q8t= row[0]   
+
+            q9 = """SELECT COUNT(*) 
+                    FROM "J.POULOS".City"""
+            cursor.execute(q9)
+            for row in cursor:
+                q9t= row[0]   
+
+            q10 = """SELECT COUNT(*) 
+                    FROM "J.POULOS".City_pop"""
+            cursor.execute(q10)
+            for row in cursor:
+                q10t= row[0]       
+            
+            str1 = "Tuples in Accident table: " + str(q1t) + "\n"
+            str2 = "Tuples in State table: " + str(q2t) + "\n"
+            str3 = "Tuples in State_pop table: " + str(q3t) + "\n"
+            str4 = "Tuples in State_fund table: " + str(q4t) + "\n"
+            str5 = "Tuples in State_veh table: " + str(q5t) + "\n"
+            str6 = "Tuples in State_lic_drivers table: " + str(q6t) + "\n"
+            str7 = "Tuples in County: " + str(q7t) + "\n"
+            str8 = "Tuples in County_pop: " + str(q8t) + "\n"
+            str9 = "Tuples in City table: " + str(q9t) + "\n"
+            str10 = "Tuples in City_pop table: " + str(q10t) + "\n"
+        
+            sum = q1t + q2t + q3t + q4t + q5t + q6t + q7t + q8t + q9t + q10t
+            str10 = "\nTotal Tuples: " + str(sum) + "\n"
+
+            st.sidebar.text_area('About', 
+                str1 + str2 + str3 + str4 + str5 + 
+                str6 + str7 + str8 + str9 + str10,
+                height = 300
+            )
+
     def run(self):
         
         l1 = Image.open('images/logo.png')
@@ -54,18 +141,7 @@ class Home(HydraHeadApp):
         st.sidebar.header('Select Time', anchor = None)
         st.sidebar.time_input('Select Time', datetime.time(8, 45))
 
-        # Text box
-        st.sidebar.header('Traffic Pro', anchor = None)
-        txt = st.sidebar.text_area('About', 
-            "TrafficPro is a traffic accident data visualization tool designed for"
-            "professionals in many fields from public safety to civil engineering."
-            "TrafficPro allows users to analyze trends about traffic accidents"
-            "occurring in the United States from 2016-2019."
-    
-            "TrafficPro integrates this accident data along with state demographic"
-            "data to allow users to visualize trends in areas of interest.",
-            height = 300
-        )
+        self.dbstats()
 
         """
         This section is for the main page elements
