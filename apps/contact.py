3695fbcc5a260e418c919e6b26d0a40ca9fe7b2f
John-Poulos <j.poulos@ufl.edu>
2022-04-05 13:10:53 -0400

    Update contact.py
    Contact page complete

--- apps/contact.py
+++ apps/contact.py
@@ -1,9 +1,17 @@
 import streamlit as st
 from hydralit import HydraHeadApp
+from db import db_conn as oracle_db
+from PIL import Image
+import textwrap
+traffic = Image.open('images/banner.png')
 
 
 class Contact(HydraHeadApp):
-
-    def run(self):
-        st.title("Contact")
-        # this is where code for page will be
+	def run(self):		
+		st.title("Contact")
+		st.image(traffic, width = 512)
+		st.write("Contact us with any inquiries:  \n\n")			
+		st.write("Jeremiah Brookes    - Lead Designer:      j.brookes@ufl.edu ")
+		st.write("Bryan Kristofferson - Back End Architect: bkristofferson@ufl.edu ")
+		st.write("John Poulos         - Project Management: j.poulos@ufl.edu ")
+		st.write("Clyde Slichter      - Database Engineer:  cnslichter@ufl.edu ")
