5774b01b2a76faaa730e2ad4cdf63db653ab2c2a
cnnslichter <77916120+cnnslichter@users.noreply.github.com>
2022-04-07 01:43:06 -0400

    Got interactive widget working, its not pretty but works

--- apps/state.py
+++ apps/state.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from hydralit import HydraHeadApp
 import matplotlib.pyplot as plt
+import pandas as pd
+from db import db_conn as oracle_db
+import ads
+
 from io import BytesIO
 
 
@@ -14,20 +18,52 @@ class State(HydraHeadApp):
         dog = ["happy", "happy", "happy", "happy", "bored", "bored"]
         activity = ["combing", "drinking", "feeding", "napping", "playing", "washing"]
 
-        # width = st.sidebar.slider("plot width", 0.1, 25., 3.)
+        #st.button("button")
+        # width = st.sidebar.slider("plot width", 2016, 2020, key="fred")
+        # year = width
         # #width = st.sidebar.slider("year", 2016, 2020, key='interval')
-        # print(width)
+        #print(width)
         #
-        # height = st.sidebar.slider("plot height", 0.1, 25., 1.)
-        #
-        # fig, ax = plt.subplots(figsize=(width, height))
+        #height = st.sidebar.slider("plot height", 2016, 2020, key="jeff")
+        # height = 1
+        # #
+        # fig, ax = plt.subplots(figsize=(width - (width-1), height-(height - 1)))
         # ax.plot(activity, dog, label="dog")
         # ax.plot(activity, cat, label="cat")
         # ax.legend()
-        #
+        # #
         # buf = BytesIO()
         # fig.savefig(buf, format="png")
         # st.image(buf)
 
-        age = st.slider('Choose a year', 2016, 2020)
-        st.write('You chose', age)
+        year = st.sidebar.slider("year", 2016, 2020)  # 👈 this is a widget
+
+        # count = 0
+        #
+        # year = st.slider('Choose a year', 2016, 2020, key=count)
+        # st.write('You chose', year)
+        # st.write("Will this show?")
+        #
+        query = f"""SELECT COUNT(ID) AS total, state_name
+                    FROM "J.POULOS".accident
+                    WHERE start_time IN (
+                        SELECT start_time FROM "J.POULOS".accident
+                        WHERE EXTRACT(year FROM start_time) = {year})
+                        GROUP BY state_name
+                    ORDER BY total DESC
+                    FETCH FIRST 8 ROWS ONLY"""
+        # st.write(query)
+        # # plt.figure(figsize=(10, 6))
+        plt.title('Top 8 states with most accidents')
+        plt.xlabel('State', fontsize=12)
+        plt.ylabel('Amount')
+        plt.xticks(fontsize=6)
+        plt.yticks(fontsize=8)
+        #count += 1
+        df = pd.read_sql(query, con=oracle_db.connection)
+        #st.write(df)
+
+        fig = plt.bar(df['STATE_NAME'], df['TOTAL'])
+        st.pyplot(fig=plt)
+
+        st.write(df)
